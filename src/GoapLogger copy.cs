c9e9f9dc5146afd6f4ce378c6cc0ee1969a2efc2
Simon van der Weele <simon@rightbrain.com.mt>
2018-02-19 13:12:19 +0100

    test

---
+++ src/GoapLogger copy.cs
@@ -0,0 +1,49 @@
+﻿namespace AI.Goap
+{
+    public static class GoapLogger
+    {
+
+        private enum DebugMode
+        {
+            OFF,
+            ERRORS,
+            WARNINGS,
+            ALL
+        }
+
+
+
+        private static DebugMode m_debugMode = DebugMode.WARNINGS;
+
+        public static void Log(string log)
+        {
+            if (m_debugMode != DebugMode.ALL)
+            {
+                return;
+            }
+
+            UnityEngine.Debug.Log(log);
+        }
+
+        public static void LogError(string log)
+        {
+            if (m_debugMode == DebugMode.OFF)
+            {
+                return;
+            }
+
+            UnityEngine.Debug.LogError(log);
+        }
+
+
+        public static void LogWarning(string log)
+        {
+            if (m_debugMode != DebugMode.WARNINGS && m_debugMode != DebugMode.ALL)
+            {
+                return;
+            }
+
+            UnityEngine.Debug.LogWarning(log);
+        }
+    }
+}
